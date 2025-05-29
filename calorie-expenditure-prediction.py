d8629cc734b31206974fad75f7f6c3f09335004a
yeonseokcho <ikgastph@gmail.com>
2025-05-29 20:42:21 +0900

    Create calorie-expenditure-prediction.py

---
+++ calorie-expenditure-prediction.py
@@ -0,0 +1,14 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+import warnings
+warnings.filterwarnings("ignore")
